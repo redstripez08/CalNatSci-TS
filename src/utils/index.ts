92af9be11a2dc43593a5ea3c32c886fced685ab0
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-02-27 23:18:48 +0800

    Changed charChecker to charCounter

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,9 @@
-import _charChecker from "./charChecker";
+import _charCounter from "./charCounter";
 // import _checkNodeEnv from "./checkNodeEnv";
 import _axiosErrorHandler from "./axiosErrorHandler";
 import _randomHex from "./randomHex";
 
-export const charChecker = _charChecker;
+export const charCounter = _charCounter;
 // export const checkNodeEnv = _checkNodeEnv;
 export const axiosErrorHandler = _axiosErrorHandler;
 export const randomHex = _randomHex;
