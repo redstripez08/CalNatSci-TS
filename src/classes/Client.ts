278a3378d1a20d81891c44d32ae1edc252244530
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-02-28 13:15:46 +0800

    Moved Client class to classes directory

---
+++ src/classes/Client.ts
@@ -0,0 +1,7 @@
+import * as Discord from "discord.js";
+import { Command } from "../typings";
+
+export default class Client extends Discord.Client {
+    commands!: Discord.Collection<Command["name"], Command>;
+    prefix!: string;
+}
--- src/classes/index.ts
+++ src/classes/index.ts
@@ -1,3 +1,5 @@
 import _Link from "./Link";
+import _Client from "./Client";
 
 export const Link = _Link;
+export const Client = _Client;
