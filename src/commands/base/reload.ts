b080090eb3da2de27a3931ae0355b39ebd6a82d5
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-02-28 13:13:52 +0800

    Created reload command that reloads other commands

---
+++ src/commands/base/reload.ts
@@ -0,0 +1,31 @@
+import Client from "../../classes/Client";
+import { Command } from "../../typings";
+
+export default {
+    name: "reload",
+    aliases: ["r"],
+    description: "Reloads a command",
+    usage: "<Command>",
+    cooldown: 5,
+    guildOnly: false,
+    argsRequired: true,
+    rolesRequired: [],
+    execute(message, args) {
+        const commandName = args[0].toLowerCase();
+        const _client: any = message.client;
+        const client: Client = _client;
+        const command = client.commands.get(commandName) ?? client.commands.find(cmd => cmd.aliases.includes(commandName));
+
+        if (!command) return message.channel.send(`\`${commandName}\` not Found!`);
+
+        try {
+            delete require.cache[require.resolve(`./${command.name}.js`)];
+            const newCommand = require(`./${command.name}.js`);
+            client.commands.set(newCommand.name, newCommand);
+            return message.channel.send(`Command \`${command.name}\` was reloaded!`);
+        } catch (error) {
+            console.error(error);
+	        message.channel.send(`There was an error while reloading a command \`${command.name}\`:\n\`${error.message}\``);
+        }
+    }
+} as Command;
