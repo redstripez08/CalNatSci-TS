0efb8da96797d0a855fadb93d5c19cbe5bb40c50
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-03-01 22:28:52 +0800

    Snipe command to get deleted message

---
+++ src/commands/base/snipe.ts
@@ -0,0 +1,38 @@
+import { Command } from "../../typings";
+import { PrismaClient } from "@prisma/client";
+import { MessageEmbed } from "discord.js";
+import { randomHex } from "../../utils";
+
+const prisma = new PrismaClient();
+
+export default {
+    name: "snipe",
+    aliases: [],
+    description: "Deleted Message Sniper",
+    usage: null,
+    cooldown: 0,
+    guildOnly: false,
+    argsRequired: false,
+    rolesRequired: [],
+    async execute(message) {
+        try {
+            prisma.$connect();
+            const snipedObject = await prisma.snipes.findFirst({
+                where: {
+                    id: 1
+                }
+            });
+            
+            const embed = new MessageEmbed()
+                .setTitle(snipedObject?.author)
+                .setDescription(snipedObject?.content)
+                .setColor(randomHex());
+
+            message.channel.send(embed);
+        } catch (error) {
+            console.error(error);
+            message.channel.send(error);
+        }
+        prisma.$disconnect();
+    }
+} as Command;
