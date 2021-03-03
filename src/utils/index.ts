217ac1553cb4ba8ea28b3c6cfd68124de8871aed
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-03-04 00:48:37 +0800

    Updated with new utilities

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,11 @@
-import _charCounter from "./charCounter";
-// import _checkNodeEnv from "./checkNodeEnv";
 import _axiosErrorHandler from "./axiosErrorHandler";
+import _charCounter from "./charCounter";
+import _checkNodeEnv from "./checkNodeEnv";
+import _getUserFromMention from "./getUserFromMention";
 import _randomHex from "./randomHex";
 
-export const charCounter = _charCounter;
-// export const checkNodeEnv = _checkNodeEnv;
 export const axiosErrorHandler = _axiosErrorHandler;
+export const charCounter = _charCounter;
+export const checkNodeEnv = _checkNodeEnv;
+export const getUserFromMention = _getUserFromMention;
 export const randomHex = _randomHex;
