16dbc66f814baa7bf08df3161da6cb9fbc3ff3c5
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-03-04 00:47:50 +0800

    Checks Node Environment.

---
+++ src/utils/checkNodeEnv.ts
@@ -0,0 +1,9 @@
+type Envs = "dev" | "production";
+
+/**
+ * Checks for Node Environment
+ * @param env Environment to be checked
+ */
+export default function (env: Envs): boolean {
+    return process.env.NODE_ENV === env;
+}
