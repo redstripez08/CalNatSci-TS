849b8d118e1ec2ccfe69efa57eb23f9dbc02818d
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-03-07 17:10:53 +0800

    Sends Some stuff at specific tim

---
+++ src/commands/ready/sendVerses.ts
@@ -0,0 +1,44 @@
+import { WebhookClient } from "discord.js";
+import { DateTime } from "luxon";
+import prisma from "../../classes/PrismaClient";
+import { ReadyCommand } from "../../typings";
+import { randomHex } from "../../utils";
+
+const { VERSES_WEBHOOK_ID, VERSES_WEBHOOK_TOKEN, IANA_TIMEZONE } = process.env;
+
+if (!VERSES_WEBHOOK_ID || !VERSES_WEBHOOK_TOKEN || !IANA_TIMEZONE) {
+    throw new Error("Webhook Token or ID not found!");
+}
+
+const webhook = new WebhookClient(VERSES_WEBHOOK_ID, VERSES_WEBHOOK_TOKEN);
+
+let sentBool: boolean = false;
+export default {
+    name: "sendverses",
+    execute() {
+        setInterval(async () => {
+            const date = DateTime.fromJSDate(new Date()).setZone(IANA_TIMEZONE);
+        
+            if (date.hour === 8 && !sentBool) {
+                const verses = await prisma.verses.findFirst({
+                    where: {
+                        id: date.day
+                    }
+                });
+
+                await webhook.send(null, {
+                    embeds: [
+                        {
+                            title: verses?.title,
+                            description: verses?.content,
+                            color: randomHex()
+                        }
+                    ]
+                });
+
+                sentBool = true;
+                setTimeout(() => sentBool = false, 3630 * 1000);
+            }
+        }, 60 * 1000);
+    }
+} as ReadyCommand;
