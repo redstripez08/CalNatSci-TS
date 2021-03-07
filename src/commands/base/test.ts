ec199232157c060459032ca89487fd96febb85f6
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-03-07 17:06:31 +0800

    Generic command for testing stuff

---
+++ src/commands/base/test.ts
@@ -0,0 +1,22 @@
+import { Command } from "../../typings";
+
+/**
+ * Just a Command for Testing stuff
+ */
+export default {
+    name: "test",
+    aliases: ["t"],
+    description: "Generic test stuff",
+    usage: null,
+    cooldown: 0,
+    guildOnly: false,
+    argsRequired: false,
+    rolesRequired: [],
+    async execute(message, args) {
+        try {
+            message.channel.send(args.join(" "));
+        } catch (error) {
+            console.error(error);   
+        }
+    }
+} as Command;
