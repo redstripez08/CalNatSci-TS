1c08d1037ab855be24dea35582ed7434b318b056
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-03-09 12:15:52 +0800

    Gets a random Waifu or a waifu filtered by category

---
+++ src/commands/base/waifu.ts
@@ -0,0 +1,46 @@
+import axios, { AxiosResponse } from "axios";
+import { Link } from "../../classes";
+import { Command } from "../../typings";
+import { axiosErrorHandler, randomHex } from "../../utils";
+import { MessageEmbed } from "discord.js";
+
+interface Response extends AxiosResponse {
+    data: {
+        url: string;
+    }
+}
+
+export default {
+    name: "waifu",
+    aliases: [],
+    description: "Gets a random waifu pic. Uses https://waifu.pics API. Find more categories there.",
+    usage: null,
+    cooldown: 0,
+    guildOnly: false,
+    argsRequired: false,
+    rolesRequired: [],
+    async execute(message, args) {
+        try {
+            const link = new Link(`/api/sfw/${args[0] ? args[0].toLowerCase() : "waifu"}`, "https://waifu.pics", {
+                headers: {
+                    "Accept": "application/json"
+                }
+            });
+
+            const res: Response = await axios.get(link.href, {headers: link.headers});
+            const embed = new MessageEmbed()
+                .setImage(res.data.url)
+                .setColor(randomHex());
+
+            message.channel.send(embed);
+        } catch (error) {
+            if (error.isAxiosError) {
+                if (error.response.status === 404) {
+                    return message.channel.send(`There was an error!\n\`${error.response.status} || ${error.response.statusText}\n${JSON.stringify(error.response.data)}\``);
+                }
+                return axiosErrorHandler(message, error);
+            }
+            console.error(error);
+        }
+    }
+} as Command;
