af928315e8a2f5742e401cabefde239ecad7d149
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-03-26 12:39:37 +0800

    Moved Prisma Connection here

--- src/classes/PrismaClient.ts
+++ src/classes/PrismaClient.ts
@@ -1,5 +1,16 @@
 import { PrismaClient } from "@prisma/client";
+import { checkNodeEnv } from "../utils";
 
 const prisma = new PrismaClient();
+
+try {
+    // Connect to Prisma Database
+    prisma.$connect();
+    console.log("[Prisma 2 | SQLite3] Connected to Database.db");
+} catch (error) {
+    if (checkNodeEnv("production")) throw new Error(error); 
+    console.error(error);
+}
+
 
 export default prisma;
