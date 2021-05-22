313d56125008d146ba9fdc2a92d78793b9586185
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-05-23 01:15:05 +0800

    Added EditSnipes Command

---
+++ src/commands/base/editsnipe.ts
@@ -0,0 +1,34 @@
+import { Command } from "../../typings";
+import prisma from "../../classes/PrismaClient";
+import { MessageEmbed } from "discord.js";
+import { randomHex } from "../../utils";
+
+export default {
+    name: "editsnipe",
+    aliases: [],
+    description: "Edited Message Sniper",
+    usage: null,
+    cooldown: 0,
+    guildOnly: false,
+    argsRequired: false,
+    rolesRequired: [],
+    async execute(message) {
+        try {
+            const snipedObject = await prisma.editSnipes.findFirst({
+                where: {
+                    id: 1
+                }
+            });
+
+            const embed = new MessageEmbed()
+                .setTitle(snipedObject?.author)
+                .setDescription(snipedObject?.content)
+                .setColor(randomHex());
+
+            message.channel.send(embed);
+        } catch (error) {
+            console.error(error);
+            message.channel.send(error);
+        }
+    }
+} as Command;
