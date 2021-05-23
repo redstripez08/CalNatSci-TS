c72f44aaf49fcad0344a17e204ad41b5d4a29bb3
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-05-23 19:19:33 +0800

    Instantiated a Client Class here

--- src/classes/Client.ts
+++ src/classes/Client.ts
@@ -1,7 +1,26 @@
 import * as Discord from "discord.js";
 import { Command } from "../../typings";
 
-export default class Client extends Discord.Client {
-    commands!: Discord.Collection<Command["name"], Command>;
-    prefix!: string;
+interface ClientOptions extends Discord.ClientOptions {
+    prefix: string;
 }
+
+export class Client extends Discord.Client {
+    public commands!: Discord.Collection<Command["name"], Command>;
+    private _prefix: string;
+
+    constructor(options: ClientOptions) {
+        super(options);
+        this._prefix = options.prefix;
+    }
+    
+    public get prefix(): string {
+        return this._prefix;
+    }
+    
+}
+
+export default new Client({
+    ws: {intents: Discord.Intents.ALL},
+    prefix: process.env.PREFIX ?? "t!"
+});
