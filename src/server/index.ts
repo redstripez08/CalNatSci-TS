273b614c926739cf5c8323c4f75d4c1f1b9460da
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-06-07 00:42:53 +0800

    Created GraphQL endpoint with Apollo and Nexus

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,11 @@
+import { ApolloServer } from "apollo-server-express";
 import express from "express";
 import * as fs from "fs";
 import * as path from "path";
+import * as types from "./schema";
+import { makeSchema } from "nexus";
 import { Route } from "../../typings";
+import prisma from "../classes/PrismaClient";
 
 const { PORT = 80, SERVER_AUTH } = process.env;
 const app = express();
@@ -13,12 +17,34 @@ app.use((req, res, next) => {
     next();
 });
 
-const routeFiles = fs.readdirSync(path.resolve(__dirname, "./routes/")).filter(file => /\.ts$|\.js$/.test(file));
+const routeFiles = fs.readdirSync(path.resolve(__dirname, "./routes/")).filter(file => file.endsWith(".js"));
 
 for (const routeFile of routeFiles) {
     const { path, router }: Route = require(`./routes/${routeFile}`).default;
     app.use(path, router);
 }
 
+const schema = makeSchema({
+    types,
+    outputs: {
+        typegen: path.resolve(__dirname, "./nexus-typegen.ts"),
+        schema: path.resolve(__dirname, "./schema/schema.graphql")
+    },
+    contextType: {
+        module: path.resolve(__dirname, "./schema/context.ts"),
+        export: "Context"
+    }
+});
 
-app.listen(PORT, () => console.log(`[HTTP/1.1 | Server] Listening to port ${PORT}`));
+(async () => {    
+    const server = new ApolloServer({schema, 
+        context: {
+            db: {
+                verses: await prisma.verses.findMany()
+            }
+        }
+    });
+
+    server.applyMiddleware({app});
+    app.listen(PORT, () => console.log(`[HTTP/1.1 | Server] Listening to port ${PORT}`));
+})();
