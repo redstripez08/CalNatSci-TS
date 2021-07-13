0ff2c04ba51a5b56316f123f20b0cbe5a144be41
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-07-13 22:54:34 +0800

    Promisified bot start

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,26 @@
-import fs from "fs";
+import fsp from "fs/promises";
 import path from "path";
 import { EventHandler } from "../typings";
 import client from "./classes/Client";
 
-const eventHandlers = fs.readdirSync(path.resolve(__dirname, "./events/")).filter(file => file.endsWith(".js"));
+(async () => {
+    const eventHandlers = (
+        await fsp.readdir(path.resolve(__dirname, "./events/"))
+    ).filter(file => file.endsWith(".js"));
 
-for (const eventHandler of eventHandlers) {
-    const event: EventHandler = require(`./events/${eventHandler}`).default;
+    for (const eventHandler of eventHandlers) {
+        const event: EventHandler = (await import(`./events/${eventHandler}`))
+            .default;
 
-    // Either client.on() or client.once()
-    client[event.type](event.name, (...args) => event.handler(client, ...args));
-}
+        // Either client.on() or client.once()
+        client[event.type](event.name, (...args) =>
+            event.handler(client, ...args)
+        );
+    }
 
-
-try {
-    client.login();
-} catch (error) {
-    throw new Error(error);
-}
+    try {
+        await client.login();
+    } catch (error) {
+        throw new Error(error);
+    }
+})();
