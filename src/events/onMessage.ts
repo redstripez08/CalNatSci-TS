82aa9070752edf3acf1df914779602f81f407488
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-07-14 17:34:37 +0800

    Refactored code into own functions

--- src/events/onMessage.ts
+++ src/events/onMessage.ts
@@ -1,61 +1,124 @@
 import { Collection, Message } from "discord.js";
 import { EventHandler, Command } from "../../typings";
 import client from "../classes/Client";
-import fs from "fs";
+import fsp from "fs/promises";
 import path from "path";
 
-const commands = client.commands = new Collection();
+client.setCommands(new Collection());
+const commands = client.commands;
 const cooldowns = new Collection<string, Collection<string, number>>();
-const base = fs.readdirSync(path.resolve(__dirname, "../commands/base/")).filter(file => file.endsWith(".js"));
 
-for (const commandFile of base) {
-    const command: Command = require(`../commands/base/${commandFile}`).default;
-    commands.set(command.name, command);
+(async () => {
+    const base = fsp.readdir(path.resolve(__dirname, "../commands/base/"));
+
+    for (const commandFile of await base) {
+        const commandImport = await import(`../commands/base/${commandFile}`);
+        const command: Command = commandImport.default;
+        commands.set(command.name, command);
+    }
+})();
+
+function isValidMessage(message: Message) {
+    const invalidMsg =
+        message.author?.bot ||
+        !message.content
+            .slice(0, client.prefix.length)
+            .toLowerCase()
+            .startsWith(client.prefix);
+
+    if (invalidMsg) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
+function parseCommand(message: Message) {
+    const args = message.content
+        .slice(client.prefix.length)
+        .trim()
+        .split(/ +/g);
+
+    const commandName = args.shift()?.toLowerCase();
+
+    // Occurs if only prefix is typed without a command.
+    if (!commandName) {
+        const msg =
+            "You need to supply a command, not just the prefix dummy." +
+            `\nTry \`${client.prefix}help\``;
+
+        message.channel.send(msg);
+        return false;
+    }
+
+    const command =
+        commands.get(commandName) ||
+        commands.find(cmd => cmd.aliases.includes(commandName));
+
+    if (!command) return false;
+
+    return { command, args };
+}
 
 export default {
     name: "message",
     type: "on",
     async handler(_, message: Message) {
-        if (message.author?.bot || !message.content.slice(0, client.prefix.length).toLowerCase().startsWith(client.prefix)) return;
-        const args = message.content.slice(client.prefix.length).trim().split(/ +/g);
-        const commandName = args.shift()?.toLowerCase();
-
-        // Occurs if only prefix is typed without a command.
-        if (!commandName) {
-            return message.channel.send(`You need to supply a command, not just the prefix dummy.\nTry \`${client.prefix}help\``);
+        if (!isValidMessage(message)) {
+            return;
         }
 
-        const command = commands.get(commandName) || commands.find(cmd => cmd.aliases.includes(commandName));
-        if (!command) return;
+        const parsedCommand = parseCommand(message);
+
+        if (!parsedCommand) {
+            return;
+        }
+
+        const { command, args } = parsedCommand;
 
         // Checks if command works only for Servers
         if (command.guildOnly && message.channel.type !== "text") {
-            return message.reply(`\`${client.prefix}${command.name}\` only works for servers.`);
+            return message.reply(
+                `\`${client.prefix}${command.name}\` only works for servers.`
+            );
         }
 
         // Checks if arguments are required and returns when args are required and none are found,
         if (command.argsRequired && !args.length) {
-            const reply = `**Args required!**${command.usage ? `\nUsage: \`${client.prefix}${commandName} ${command.usage}\`` : ""}`;
+            const usage = command.usage
+                ? `\nUsage: \`${client.prefix}${command.name} ${command.usage}\``
+                : "";
+
+            const reply = `**Args required!**${usage}`;
             return message.channel.send(reply);
         }
 
+        // cooldownHandler(message, command);
+
         // Sets cooldown for each command
         if (!cooldowns.has(command.name)) {
-            cooldowns.set(command.name, new Collection()); 
+            cooldowns.set(command.name, new Collection<string, number>());
         }
 
         const now = Date.now();
+        // Records the time command was called
         const timestamps = cooldowns.get(command.name);
         const cooldownAmount = command.cooldown * 1000;
-        if (!timestamps) return console.error("Timestamps not found for some reason despite being set in lines 81-83");
+
+        if (!timestamps) {
+            throw new Error("Timestamps not found for some reason");
+        }
 
         if (timestamps.has(message.author.id)) {
-            const expirationTime = (timestamps.get(message.author.id) ?? 0) + cooldownAmount;
+            const expirationTime =
+                (timestamps.get(message.author.id) ?? 0) + cooldownAmount;
+
             if (now < expirationTime) {
                 const timeLeft = (expirationTime - now) / 1000;
-                const text = `Please wait **${timeLeft.toFixed(1)}** more second(s) before reusing the \`${command.name}\` command.`;
+                const timeLeftToFixed = timeLeft.toFixed(1);
+                const text = `Please wait **${timeLeftToFixed}** more second(s) before reusing 
+                the \`${command.name}\` command.`;
+
                 return message.channel.send(text);
             }
         }
@@ -64,14 +127,20 @@ export default {
         try {
             command.execute(message, args);
             // Set Cooldown.
-            // If cooldown is 0, it will not set since it is instantaneous. I think this makes it less expensive to run though I may be wrong
+            // If cooldown is 0, it will not set since it is instantaneous.
+            // I think this makes it less expensive to run though I may be wrong
             if (cooldownAmount !== 0) {
                 timestamps.set(message.author.id, now);
-                setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+                setTimeout(
+                    () => timestamps.delete(message.author.id),
+                    cooldownAmount
+                );
             }
         } catch (error) {
             console.error(error);
-            message.channel.send(`There was an error executing the command!\n\`${error}\``);
+            message.channel.send(
+                `There was an error executing the command!\n\`${error}\``
+            );
         }
-    }
+    },
 } as EventHandler;
