71271d2c7c8df4df73b9ede2f9be5addf25c5eac
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-07-14 18:14:06 +0800

    Removed and gitignored dist

--- dist/commands/ready/test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    name: "test",
-    async execute() {
-        // Test stuff
-    }
-};
--- dist/server/graphql/Query.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Query = void 0;
-const nexus_1 = require("nexus");
-exports.Query = nexus_1.queryType({
-    definition(t) {
-    }
-});
